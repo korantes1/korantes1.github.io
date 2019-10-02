fe9849087171dd6069146e128c0f9d8aa0b1ca53
Kevin Orantes <54971783+korantes1@users.noreply.github.com>
2019-10-01 19:32:57 -0700

    Add files via upload

---
+++ hw2/effects.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+$('.menu-toggle').on('click', function() {
+
+  $('ul').toggleClass('opening');
+  $(this).toggleClass('open');
+
+});
+
+});
+
+$(window).on('resize',function(){location.reload();});
+
+function toggleTabs(evt, tabName) {
+	
+	if ($(window).width() < 480) {
+	
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+	else 
+		
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "block";
+  }
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+		
