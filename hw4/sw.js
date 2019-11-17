fb4d32e3ebf8dd72e9f9fcf45a91583cd4590c38
Kevin Orantes <54971783+korantes1@users.noreply.github.com>
2019-11-16 22:32:09 -0800

    Add files via upload

--- hw4/sw.js
+++ hw4/sw.js
@@ -13,10 +13,26 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', event => {
 	const req = event.request;
-	event.respondWith(cacheFirst(req))
+	const url = new URL(req.url);
+if(url.origin = location.oring){
+	event.respondWith(cacheFirst(req));
+} else {
+	event.respondWith(networkFirst(req));
+}
 });
 
 async function cacheFirst(req) {
 	const cachedResponse = await caches.match(req);
 	return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+	const cache = await caches.open('bg-dynamic');
+	try {
+		const res = await fetch(req);
+		cache.put(req, res.clone());
+		return res;
+	} catch (error) {
+		return await cache.match(req);		
+	}
 }
