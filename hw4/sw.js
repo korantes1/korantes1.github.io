32357ce3becba0b31494f9a3d531ff83e05df12f
Kevin Orantes <54971783+korantes1@users.noreply.github.com>
2019-11-16 20:54:53 -0800

    Add files via upload

--- hw4/sw.js
+++ hw4/sw.js
@@ -1,10 +1,7 @@
-importScripts('https://code.jquery.com/jquery-3.4.1.js')
+self.addEventListener('install', event => {
+	console.log('install');
+});
 
-const staticAssets = [
-  './',
-  './app.js',
-  './styles.css',
-  './fallback.json',
-  './img/lightgold.jpg',
-  './img/lightblue.jpg',
-];
+self.addEventListener('fetch', event => {
+	console.log('fetch');
+});
